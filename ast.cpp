b84dd941ab88dd8c039e947e3a3b22baabaebae3
Ruixin Bao <peter.bao@mail.utoronto.ca>
2018-11-06 10:16:36 -0500

    Refactor: no need the ugly casting..
    Welp, my friend helped me figure out the reason why the compilation failed.
    
    And, refactor the classes out of the .h, making .h file only contain
    the shared configurations(functions, enum and class definitions)
    
    The refactored portion is then put inside ast.cpp

--- ast.cpp
+++ ast.cpp
@@ -8,43 +8,193 @@
 #define DEBUG_PRINT_TREE 0
 
 
-void* ast = NULL;
+/*========================================Initial Declarations===================*/
+class Type : public Node {
+    public:
+        virtual void visit(Visitor &visitor){
+            visitor.visit(this);
+        }
+        std::string type_name;
 
-void* ast_allocate(NodeKind type, ...){
+        Type(std::string type){
+            this->type_name = type;
+        }
+};
+
+class Identifier : public Node {
+    public:
+        std::string name;
+        virtual void visit(Visitor &visitor){
+            visitor.visit(this);
+        }
+        Identifier(std::string val){
+            this->name = val;
+        }
+};
+
+class Declaration : public Node {
+    public:
+        Type *t;
+        Identifier *i;
+        std::string initial_val;
+        virtual void visit(Visitor &visitor) {
+            visitor.visit(this);
+            // i->visit(visitor);
+            t->visit(visitor);
+            if (initial_val.length() != 0)
+                cout << initial_val;
+            printf(")\n");
+        }
+};
+
+class Declarations : public Node {
+    public:
+        Declarations *declarations;
+        Declaration *declaration;
+        virtual void visit(Visitor &visitor) {
+            if (declaration != NULL)
+                declaration->visit(visitor);
+            if (declarations != NULL)
+                declarations->visit(visitor);
+        }
+};
+
+
+class  Scope: public Node {
+    public:
+        Declarations* d;
+        virtual void visit(Visitor &visitor) {
+            visitor.visit(this);
+            if (d)
+                d->visit(visitor);
+            //s->visit(visitor);
+            printf(")\n");
+        }
+};
+/*=========================================END OF INITIAL DECLARATIONS======================*/
+
+/*=========================Beginning of STATEMENT class===================================*/
+class Statements {
+
+    public:
+        Statements *statements;
+        Statement  *statement;
+};
+
+class Statement {
+    public:
+        StatementType statement_type;
+        Statement(StatementType st){
+            this->statement_type = st;
+        }
+};
+
+class AssignStatement : public Statement {
+    public:
+        Variable *variable;
+        Expression *expression;
+};
+
+class IfStatement : public Statement {
+    public:
+        Expression *expression;
+        Statement *statement;
+        Statement *else_statement;
+
+};
+class Nested_scope : public Statement {
+    public:
+        Statement *statement;
+};
+/*================END OF STATEMENT CLASS================*/
+
+/*================Start of Expression classes===========*/
+class Expression : public Node {
+    public:
+        ExpressionType expression_type;
+        Expression(ExpressionType et){
+            this->expression_type = et;
+        }
+};
+
+class ConstructorExpression : public Expression {
+    public:
+        Constructor *constructor;
+};
+
+class LiteralExpression : public Expression {
+    public:
+        int int_literal;
+        float float_literal;
+        bool bool_literal;
+};
+
+class UnaryExpression : public Node {
+    public:
+        string op;
+        Expression *right_expression;
+};
+
+class BinaryExpression : public UnaryExpression {
+    public:
+        Expression *left_expression;
+};
+
+class Constructor : public Node {
+    public:
+        Type *type;
+        Arguments *args;
+};
+
+class Arguments : public Node {
+    public:
+        Arguments *args;
+        Expression *expression;
+};
+
+class Variable : public Node {
+    public:
+        Identifier *id;
+};
+/*===========================End of expression classes========================*/
+
+node* ast = NULL;
+
+node* ast_allocate(NodeKind type, ...){
 
     va_list args;
 
-    void *ret_node = NULL;
+    node *ret_node = NULL;
     va_start(args, type);
     switch(type) {
         case SCOPE_NODE: {
             Scope* scope = new Scope();
-            scope->d = reinterpret_cast<Declarations*>(va_arg(args, void*));
+            scope->d = va_arg(args, Declarations*);
 
-            ret_node = reinterpret_cast<void*>(scope);
+            ret_node = scope;
             break;
         }
 
         case DECLARATIONS_NODE:{
             Declarations *declarations = new Declarations();
-            declarations->declarations = reinterpret_cast<Declarations*>(va_arg(args, void*));
-            declarations->declaration =  reinterpret_cast<Declaration*>(va_arg(args, void*));
+            declarations->declarations = va_arg(args, Declarations*);
+            declarations->declaration =  va_arg(args, Declaration*);
 
-            ret_node = reinterpret_cast<void*>(declarations);
+            ret_node = declarations;
             break;
         }
 
         case DECLARATION_NODE: {
             Declaration *declaration = new Declaration();
-            declaration->i = reinterpret_cast<Identifier*>(va_arg(args, void*));
-            declaration->t = reinterpret_cast<Type*>(va_arg(args, void*));
+            declaration->i = va_arg(args, Identifier*);
+            declaration->t = va_arg(args, Type*);
 
-            ret_node = reinterpret_cast<void*>(declaration);
+            ret_node = declaration;
             break;
         }
         case IDENT_NODE: {
             string val = static_cast<string>(va_arg(args, char*));
-            ret_node = reinterpret_cast<void*>(new Identifier(val));
+            ret_node = new Identifier(val);
             break;
         }
         case TYPE_NODE: {
@@ -57,12 +207,11 @@ void* ast_allocate(NodeKind type, ...){
                 assert(true && "Index for type, please check for bugs\n");
             string type = type_list[type_index][dimen_index - 1]; /* dim goes from 1 to 4 */
             cout << "Test instantiations\n" << type;
-            ret_node = reinterpret_cast<void*>(new Type(type));
+            ret_node = new Type(type);
             break;
         }
         default:
             ;
-
     }
 
     va_end(args);
@@ -70,10 +219,26 @@ void* ast_allocate(NodeKind type, ...){
 
 }
 
-
-void ast_print(void* root){
-    Node *casted_class = reinterpret_cast<Node*>(root);
+void ast_print(node* root){
     Visitor visitor;
-    casted_class->visit(visitor);
+    root->visit(visitor);
+}
+
+inline void Visitor::visit(Scope *scope){
+    printf("(SCOPE \n");
+}
+
+inline void Visitor::visit(Identifier *ident){
+    /* C++ does not supports string for printf */
+    cout << ident->name;
+    ;
+}
+
+inline void Visitor::visit(Declaration *decl){
+    printf("(DECLARATION ");
+}
+
+inline void Visitor::visit(Type *type){
+    cout << type->type_name;
 }
 
--- ast.h
+++ ast.h
@@ -8,9 +8,11 @@
 #include <assert.h>
 #include <iostream>
 
-
 using namespace std;
-// Forward declarations
+/**************************************************************************
+ *                              FORWARD DECLARATIONS                      *
+ *                                                                        *
+ *************************************************************************/
 class Scope;
 class Declaration;
 class Declarations;
@@ -19,13 +21,35 @@ class Type;
 class Node;
 class Identifier;
 
+/*----------------------------------Statement related--------------------*/
+class Statement;
+class Statements;
+class AssignStatement;
+class IfStatement;
+
+/*--------------------------------Expression related---------------------*/
+class Expression;
+class ConstructorExpression;
+class LiteralExpression;
+class Constructor;
+class UnaryExpression;
+class BinaryExpression;
+class Constructor;
+class Variable;
+class Arguments;
+
+
+typedef Node node;
+
+extern node *ast;
+
 typedef enum {
   UNKNOWN               = 0,
 
   SCOPE_NODE            = (1 << 0),
 
   EXPRESSION_NODE       = (1 << 2),
-  UNARY_EXPRESION_NODE  = (1 << 2) | (1 << 3),
+  UNARY_EXPRESSION_NODE  = (1 << 2) | (1 << 3),
   BINARY_EXPRESSION_NODE= (1 << 2) | (1 << 4),
   INT_NODE              = (1 << 2) | (1 << 5),
   FLOAT_NODE            = (1 << 2) | (1 << 6),
@@ -42,10 +66,28 @@ typedef enum {
 
   DECLARATIONS_NODE     = (1 << 15),
   DECLARATION_NODE      = (1 << 16),
+  STATEMENTS_NODE       = (1 << 17),
+  VECTOR_NODE           = (1 << 18),
+  ARGUMENTS_NODE        = (1 << 19),
+  VARIABLE_NODE         = (1 << 20)
 } NodeKind;
 
+typedef enum {
+    BOOL_EXPRESSION,
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    CONSTRUCTOR_EXPRESSION,
+    VARIABLE,
+    FUNCTION,
+    EXPRESSION,
+} ExpressionType;
 
-extern void* ast;
+typedef enum {
+    ASSIGN_STATEMENT,
+    IF_STATEMENT,
+    NESTED_SCOPE,
+    STATEMENT,
+} StatementType;
 
 class Visitor {
 public:
@@ -53,6 +95,20 @@ public:
     void visit(Scope *scope);
     void visit(Declaration *decl);
     void visit(Type *type);
+
+    void visit(Statement *stmt);
+    void visit(Statements *stmts);
+    void visit(AssignStatement *as_stmt);
+    void visit(IfStatement *if_statement);
+
+    void visit(ConstructorExpression *ce);
+    void visit(LiteralExpression *le);
+    void visit(UnaryExpression *ue);
+    void visit(BinaryExpression *be);
+
+    void visit(Constructor *c);
+    void visit(Arguments *args);
+    void visit(Variable *var);
 };
 
 class Node {
@@ -60,88 +116,7 @@ class Node {
         virtual void visit(Visitor &vistor) = 0;
 };
 
-class Type : public Node {
-    public:
-        virtual void visit(Visitor &visitor){
-            visitor.visit(this);
-        }
-        std::string type_name;
+node *ast_allocate(NodeKind type, ...);
 
-        Type(std::string type){
-            this->type_name = type;
-        }
-};
-
-class Identifier : public Node {
-    public:
-        std::string name;
-        virtual void visit(Visitor &visitor){
-            visitor.visit(this);
-        }
-        Identifier(std::string val){
-            this->name = val;
-        }
-};
-
-class Declaration : public Node {
-    public:
-        Type *t;
-        Identifier *i;
-        std::string initial_val;
-        virtual void visit(Visitor &visitor) {
-            visitor.visit(this);
-            i->visit(visitor);
-            t->visit(visitor);
-            if (initial_val.length() != 0)
-                cout << initial_val;
-            printf(")\n");
-        }
-};
-
-class Declarations : public Node {
-    public:
-        Declarations *declarations;
-        Declaration *declaration;
-        virtual void visit(Visitor &visitor) {
-            if (declaration != NULL)
-                declaration->visit(visitor);
-            if (declarations != NULL)
-                declarations->visit(visitor);
-        }
-};
-
-
-class  Scope: public Node {
-    public:
-        Declarations* d;
-        virtual void visit(Visitor &visitor) {
-            visitor.visit(this);
-            if (d)
-                d->visit(visitor);
-            //s->visit(visitor);
-            printf(")\n");
-        }
-};
-
-inline void Visitor::visit(Scope *scope){
-    printf("(SCOPE \n");
-}
-
-inline void Visitor::visit(Identifier *ident){
-    /* C++ does not supports string for printf */
-    cout << ident->name;
-    ;
-}
-
-inline void Visitor::visit(Declaration *decl){
-    printf("(DECLARATION ");
-}
-
-inline void Visitor::visit(Type *type){
-    cout << type->type_name;
-}
-
-void *ast_allocate(NodeKind type, ...);
-
-void ast_print(void *ast_root);
+void ast_print(node *ast_root);
 #endif /* AST_H_ */
