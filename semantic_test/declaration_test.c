f34a735450c88ae17beaa9ec275a234fc76363dd
Ruixin Bao <peter.bao@mail.utoronto.ca>
2018-11-08 00:02:48 -0500

    Test: added ast print tests
    Per title

---
+++ semantic_test/declaration_test.c
@@ -0,0 +1,6 @@
+{
+    int a = 3;
+    int a;
+    float b = 3.0;
+    bool a = true;
+}
---
+++ semantic_test/function_test.c
@@ -0,0 +1,7 @@
+{
+    int a = dp3(1, 2, 3);
+    int b = bvec2(3, 4);
+
+    int c = b[3];
+    int d = b;
+}
