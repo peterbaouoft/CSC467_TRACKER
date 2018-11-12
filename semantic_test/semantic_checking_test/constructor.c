a58ab7c3e0b6b60d4294aef6ce5d77cb3becaa9f
Khoi To <khoi.to@mail.utoronto.ca>
2018-11-12 18:45:23 -0500

    Added full constructor test case

---
+++ semantic_test/semantic_checking_test/constructor.c
@@ -0,0 +1,19 @@
+{
+    bool b;
+    bvec2 b2;
+
+    int i;
+    ivec2 i2;
+
+    float f;
+    vec2 f2;
+    
+    b = bool(true);
+    b2 = bvec2(true, false);
+
+    i = int(1);
+    i2 = ivec2(2,1);
+
+    f = float(1.2);
+    f2 = vec2(1.2, 3.1);
+}
