4830dd71b86855bae09f4006fd30d08bbf5be57f
Khoi To <khoi.to@mail.utoronto.ca>
2018-11-12 19:17:41 -0500

    Added new vector indexing test case

---
+++ semantic_test/semantic_checking_test/vector.c
@@ -0,0 +1,14 @@
+{
+    bvec2 b2;
+    bool b;
+    ivec4 i4;
+    int i;
+    vec3 f3;
+    float f;
+    b2 = bvec2(true, false);
+    i4 = ivec4(1, 2, 4, 2);
+    f3 = vec3(1.2,3.2,3.1);
+    
+    b = b2[6];
+    f = i4[2];
+}
